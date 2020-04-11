51b2a3f167c5022bed5561e448d2b36d45cea7b7
ishmam ali <ishmamalikhan@gmail.com>
2020-04-12 01:02:53 +0500

    navbar&bg

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hotel Management System</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link href="images/LOGO.png" type="img/icon" rel="icon">
+</head>
+<body>
+<div id="full">
+<div id="header">
+<div id="logo">
+<h1><font color="black">My Project</font></h1>
+</div>
+<div id="nav">
+<ul>
+<li><a href="#">Home</a></li>
+<li><a href="#">Contact Us</a></li>
+<li><a href="#">Book My Stay</a></li>
+<li><a href="#">Our Hotel</a></li>
+<li><a href="#">Help</a></li>
+</ul>
+</div>
+</div>
+
+    <div id="banner">
+    </div>
+    <div id="f1">
+        <table>
+            <tr>
+                <th width="20%" height="50px">Destination</th>
+                <th width="20%" height="50px">Check In Date</th>
+                <th width="20%" height="50px">Check Out Date</th>
+                <th width="20%" height="50px">Room</th>
+</tr>
+<tr>
+                <td width="20%" height="50px"><input type="text" name="d1" placeholder="Enter Your Destination"></td>
+                <td width="20%" height="50px"><input type="text" name="ci"></td>
+                <td width="20%" height="50px"><input type="text" name="co"></td>
+                <td width="20%" height="50px">    <select>
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+</select></td>
+            
+</tr>
+</table>
+</div>
+</div>
+</body>
+</html>
